c57620b61a2c9a71eddb454831eb99bf96ce34da
ShadesOfBlack <shadesofblack@mailinator.com>
2017-01-29 15:34:24 +0000

    Create CommandEngine.bat

---
+++ CommandEngine.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+SET CyanOSVersion=0010
+Title CyanDOS 0.0.1
+IF "%1"=="-ver" IF "%2"=="0010" GOTO CommandDest0010
+ECHO Unable to open file stream, Invalid parameters used.
+EXIT
+
+:CommandDest0010
+COLOR 0B
+ECHO.
+SET /P Command=::%CD%::[%TIME%]:: 
+rem ; Anode Command.
+IF "%Command%"=="ANODE" GOTO Anode
+IF "%Command%"=="A" GOTO Anode
+IF "%Command%"=="a" GOTO Anode
+IF "%Command%"=="anode" GOTO Anode
+
+rem ; Command Not On System?
+ECHO Invalid command: "%Command%" - Command Not Found.
+rem ; Prevent loop of previous command.
+SET Command=
+GOTO CommandDest0010
+
+rem ; Internal Command For Anode
+:Anode
+ECHO :::::::::::: System Information ::::::::::::
+ECHO.
+ECHO OSName : Cyan DOS Alpha
+ECHO OSFullName : cyndos.alpha
+ECHO OSVersion : 0.0.1
+ECHO OSVersionPKG : 0010
+ECHO.
+ECHO System RAM : 25MB - 50MB
+ECHO CPU Usage : 1 Ghz - 1.5 Ghz
+ECHO.
+ECHO Registered Name : %Username%
+ECHO System Directory : %cd%
+ECHO.
+ECHO :::::::::::::::::::::::::::::::::::::::::::::
+GOTO CommandDest0010
+
